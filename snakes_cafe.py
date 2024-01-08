d156fdd1d66fe1cb179c6948f033c1745e7b01f3
Xin Deng <xind1470@gmail.com>
2024-01-08 11:53:38 -0800

    first commit

---
+++ snakes_cafe.py
@@ -0,0 +1,6 @@
+print('an intro message and the menu for the restaurant')
+input1 = input('give me your order') 
+# input was redefined as a string so second print is now a string but we wanted to call it as a function
+
+print(input1)
+input2 = input("give me another order")
